d657528e134a57b9b94970602174261927f9394d
张宇隆 <609520262@qq.com>
2022-12-30 15:30:14 +0800

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ release = '1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 html_context = {
-   'source_url_prefix': 'https://github.com/609520262/Deploy-static-content-to-Pages'
+   'github_url': 'https://github.com/609520262/Deploy-static-content-to-Pages'
                }
 
 
